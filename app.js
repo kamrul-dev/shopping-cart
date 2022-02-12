77342120859f1ebde466ac0c5ff71c2cc583a31c
Kamrul Hasan <kamrulsdev@gmail.com>
2022-02-12 20:47:28 +0600

    set the input value and increased by 1

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 // button handler
 document.getElementById('case-plus').addEventListener('click', function(){
    const caseInput = document.getElementById('case-number');
-   const caseNumber = caseInput.value; 
+   const caseNumber = caseInput.value;
+   caseInput.value = parseInt(caseNumber) + 1; 
 })
