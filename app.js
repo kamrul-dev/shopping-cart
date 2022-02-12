7c451f6b959754340121a958c15fd189a7282a96
Kamrul Hasan <kamrulsdev@gmail.com>
2022-02-12 21:02:50 +0600

    Quantity increament and decrement using function to reduce code

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
+
+function updateCaseNumber(isIncreasing){
+    const caseInput = document.getElementById('case-number');
+    const caseNumber = caseInput.value;
+    if(isIncreasing == true){
+        caseInput.value = parseInt(caseNumber) + 1; 
+    }
+    else if(caseNumber > 0){
+        caseInput.value = parseInt(caseNumber) - 1;
+    }
+}
 // button handler
 document.getElementById('case-plus').addEventListener('click', function(){
-   const caseInput = document.getElementById('case-number');
-   const caseNumber = caseInput.value;
-   caseInput.value = parseInt(caseNumber) + 1; 
-})
+    updateCaseNumber(true);
+});
+document.getElementById('case-minus').addEventListener('click', function(){
+    updateCaseNumber(false);
+});
